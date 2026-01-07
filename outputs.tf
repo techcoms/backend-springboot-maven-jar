34497b6444bbd01363d1d735c6b7bea2ddf248c3
techcoms <120088252+techcoms@users.noreply.github.com>
2026-01-07 21:36:32 +0530

    Simplify output value assignment in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,5 @@
 output "ecr_repository_url" {
-  value = local.ecr_exists
-    ? data.aws_ecr_repository.existing.repository_url
-    : aws_ecr_repository.app_repo[0].repository_url
+  value = local.ecr_exists ? data.aws_ecr_repository.existing.repository_url : aws_ecr_repository.app_repo[0].repository_url
 }
 
 output "ecs_cluster_name" {
