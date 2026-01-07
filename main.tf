97525c70f275b323e45c8c5e11b728d7a7491f62
techcoms <120088252+techcoms@users.noreply.github.com>
2026-01-07 22:43:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-resource "aws_ecr_repository" "app_repo" {
-  name = var.ecr_repo_name
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
 resource "aws_ecs_cluster" "main_cluster" {
   name = var.ecs_cluster_name
 
