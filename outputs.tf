1f466935058b60b306f9f61f745d07e87313a914
techcoms <120088252+techcoms@users.noreply.github.com>
2026-01-07 21:30:15 +0530

    Modify outputs.tf for ECR repository URL logic
    Updated ECR repository URL output to handle existing repository case.

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,13 @@
 output "ecr_repository_url" {
-  description = "The URL of the ECR repository"
-  value       = aws_ecr_repository.app_repo.repository_url
+  value = local.ecr_exists
+    ? data.aws_ecr_repository.existing.repository_url
+    : aws_ecr_repository.app_repo[0].repository_url
 }
+
 output "ecs_cluster_name" {
-  description = "The name of the ECS cluster"
-  value       = aws_ecs_cluster.main_cluster.name
+  value = aws_ecs_cluster.main_cluster.name
 }
+
 output "ecs_cluster_arn" {
-  description = "The ARN of the ECS cluster"
-  value       = aws_ecs_cluster.main_cluster.arn
+  value = aws_ecs_cluster.main_cluster.arn
 }
